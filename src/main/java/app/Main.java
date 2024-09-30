083c4192a40e65feb733030275a915a176430cf9
Admin <melnukvikysa@gmail.com>
2024-09-30 16:47:43 +0300

    fahrenheit to celsius conversion

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,18 @@
 package app;
-
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Програма для конвертації фаренгейтів в цельсії, цельсіїв в фаренгейти");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Конвертація фаренгейтів в цельсії");
+        System.out.println(" Введіть температуру в фарангейтах: ");
+        double fa = scanner.nextDouble();
+        double ce;
+        ce = (fa -32 ) / 1.8;
+        System.out.println(ce);
+
         }
 }
